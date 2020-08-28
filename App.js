4e415b7b33192013b40be748817593afd9ef974c
Rodrigo Bossini <professorbossini@gmail.com>
2020-08-28 17:01:59 -0300

    Terminamos o pdf 02

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component{
+  constructor (props){
+    super (props);
+    this.state = {
+      contador: 0
+    }
+  }
+  render (){
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.contador}</Text>
+        <Button 
+          title="Incrementar"
+          onPress={() => {
+            this.setState({contador: this.state.contador + 1})
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+/*export default function App() {
+  const [outputText, setOutputText] = useState ("Texto Inicial");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{outputText}</Text>
+      <Button 
+        title="Alterar texto"
+        onPress={() => {setOutputText("Novo texto")}}
+      />
     </View>
   );
-}
+}*/
 
 const styles = StyleSheet.create({
   container: {
